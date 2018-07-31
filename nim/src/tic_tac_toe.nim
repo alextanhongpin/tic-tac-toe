c01544e94ac1243a621ba62166c8ad5fe0d75d7e
alextanhongpin <alextan220990@gmail.com>
2018-08-01 00:16:02 +0800

    chore: improve solution

---
+++ nim/src/tic_tac_toe.nim
@@ -0,0 +1,19 @@
+# Hello Nim!
+# proc main() =
+#   echo "Enter your name:"
+#   var name = readLine(stdin)
+#   while name == "":
+#     echo "What's your name?"
+#     name = readLine(stdin)
+
+
+# proc minimax(depth: string, maximizingPlayer: int) =
+#   if depth == 0 or node.isTerminal:
+#     return
+#   if maximizingPlayer:
+#     value = Inf
+#     for each child of node do
+#        value := max(value(minimax(child, depth)))
+#   else:
+
+# main()
---
+++ nim/tic_tac_toe.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.1.0"
+author        = "alextanhongpin"
+description   = "minimax tic-tac-toe in nimlang"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["tic_tac_toe"]
+
+# Dependencies
+
+requires "nim >= 0.18.0"
