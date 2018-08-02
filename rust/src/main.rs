2152fb352c3e3741ddae663cff5de3502cef780d
alextanhongpin <alex@policypal.com>
2018-08-02 19:11:37 +0800

    chore: update rust example

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,15 +2,11 @@
 
 use std::{io,f64};
 
+
 fn main() {
     println!("Hello, world!");
-    let combinations = vec![
-        vec![0,1,2], vec![3,4,5], vec![6,7,8], 
-        vec![0,3,6], vec![1,4,7], vec![2,5,8],
-        vec![0,4,8], vec![2,4,6]
-    ];
-    let mut state = vec!["", "", "","", "", "","", "", ""];
 
+    let mut state = vec!["", "", "","", "", "","", "", ""];
     loop {
         println!("Enter your move:");
         let mut choice = String::new();
@@ -28,13 +24,18 @@ fn main() {
         println!("Your move is {}", choice);
         println!("minimax is {:?}", minimax(&state, 9, false));
 
-        if game_over(&combinations, &state) {
+        if game_over(&state) {
             break;
         }
     }
 }
 
-fn game_over(combinations: &Vec<Vec<usize>>, state: &Vec<&str>) -> bool {
+fn game_over(state: &Vec<&str>) -> bool {
+   let combinations = vec![
+        vec![0,1,2], vec![3,4,5], vec![6,7,8], 
+        vec![0,3,6], vec![1,4,7], vec![2,5,8],
+        vec![0,4,8], vec![2,4,6]
+    ];
     for c in combinations {
         let combination: Vec<&str> = c.iter().map(|i| state[*i as usize]).collect();
         let pattern: &str = &combination.join("");
@@ -52,20 +53,36 @@ fn minimax(
     depth: usize, 
     maximizing_player: bool
 ) -> (usize, f64) {
+    let mut state = state.clone();
     // (move, score)
     if depth == 0 || game_over(&state) {
-        return (-1, -1.0)
+        return (0, -1.0)
     };
-    for i, v in &state.iter().enumerate() {
+    
+    let mut best_score = if maximizing_player { -f64::INFINITY } else { f64::INFINITY };
+    let mut best_move = 0;
+
+    for (i, v) in state.iter_mut().enumerate() {
         if !v.is_empty() {
             continue
         }
         let label = if maximizing_player { "x" } else { "o" };
-        let mut score = if maximizing_player { -f64::INFINITY } else { f64::INFINITY };
 
-        &state[i] = label;
-        let (_, best_score) = minimax(&state, depth - 1, !maximizing_player); 
-        &state[i] = "";
+        *v = label;
+        let mut state = &state.clone();
+        let (_, score) = minimax(&state, depth - 1, !maximizing_player); 
+        if maximizing_player {
+            if score > best_score {
+                best_score = score;
+                best_move = i;
+            }
+        } else {
+            if score < best_score {
+                best_score = score;
+                best_move = i;
+            }
+        }
+        *v = "";
     };
-    (1, f64::INFINITY)
+    (best_move, best_score)
 }
